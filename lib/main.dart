43a7cd6c84cc69f02f44aef2c5622f204e224057
madhavth <powermt2012@gmail.com>
2020-08-27 06:22:18 +0545

    added flutter screenutil

--- lib/main.dart
+++ lib/main.dart
@@ -37,7 +37,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Book Doctor',
+      title: 'Madhavth Start Repo',
       theme: ThemeData(
           fontFamily: 'Avenir',
           textTheme: TextTheme(
