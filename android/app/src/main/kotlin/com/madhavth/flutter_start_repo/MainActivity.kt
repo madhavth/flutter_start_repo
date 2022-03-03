83664705c2070960e8d17be2020fcb59ea88c6fa
madhavth <powermt2012@gmail.com>
2022-03-03 09:24:48 +0545

    fixed mainactivity platform call

--- android/app/src/main/kotlin/com/madhavth/flutter_start_repo/MainActivity.kt
+++ android/app/src/main/kotlin/com/madhavth/flutter_start_repo/MainActivity.kt
@@ -1,16 +1,22 @@
 package com.madhavth.flutter_start_repo
 
+import io.flutter.plugins.GeneratedPluginRegistrant
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
 class MainActivity: FlutterActivity() {
-    @Override
- public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
-     GeneratedPluginRegistrant.registerWith(flutterEngine);
-     new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL)
-             .setMethodCallHandler(
-                 (call, result) -> {
-                        // Your existing code
-               }
-        );
+
+    companion object {
+        const val CHANNEL = "com.madhavth.madhavth_platform_channel"
+    }
+
+ override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+    GeneratedPluginRegistrant.registerWith(flutterEngine)
+    MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL)
+             .setMethodCallHandler {
+                     _, _ ->
+                 // TODO
+             }
     }
 }
