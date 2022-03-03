ce3e3bc3f3916b5f0556b787a694ec770d2f030d
madhavth <powermt2012@gmail.com>
2022-03-03 08:39:12 +0545

    added number & file utils

---
+++ lib/utils/file_utils.dart
@@ -0,0 +1,24 @@
+import 'dart:io';
+import 'dart:math';
+
+class FileUtils {
+  
+   static Future<String> getFileSizeFromPath(String filepath, int decimals) async {
+    var file = File(filepath);
+    int bytes = await file.length();
+    if (bytes <= 0) return "0 B";
+    const suffixes = ["B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
+    var i = (log(bytes) / log(1024)).floor();
+    return ((bytes / pow(1024, i)).toStringAsFixed(decimals)) + ' ' + suffixes[i];  
+}
+
+
+    static Future<String> getFileSize(File file, int decimals) async {
+    int bytes = await file.length();
+    if (bytes <= 0) return "0 B";
+    const suffixes = ["B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
+    var i = (log(bytes) / log(1024)).floor();
+    return ((bytes / pow(1024, i)).toStringAsFixed(decimals)) + ' ' + suffixes[i];  
+}
+
+}
---
+++ lib/utils/number_utils.dart
@@ -0,0 +1,15 @@
+class NumberUtils {
+   static String k_m_b_generator(num) {
+      if (num > 999 && num < 99999) {
+        return "${(num / 1000).toStringAsFixed(1)} K";
+      } else if (num > 99999 && num < 999999) {
+        return "${(num / 1000).toStringAsFixed(0)} K";
+      } else if (num > 999999 && num < 999999999) {
+        return "${(num / 1000000).toStringAsFixed(1)} M";
+      } else if (num > 999999999) {
+        return "${(num / 1000000000).toStringAsFixed(1)} B";
+      } else {
+        return num.toString();
+      }
+    }
+}
