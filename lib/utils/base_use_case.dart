f8522326bcc26066361537172a4a716ec7ffe222
madhavth <powermt2012@gmail.com>
2022-03-27 11:29:52 +0545

    removed lint warning for usecase

--- lib/utils/base_use_case.dart
+++ lib/utils/base_use_case.dart
@@ -1,9 +1,12 @@
 import 'package:dartz/dartz.dart';
 import 'package:equatable/equatable.dart';
+
 import '../core/errors/failures.dart';
 
 // Parameters have to be put into a container object so that they can be
 // included in this abstract base class method definition.
+
+//ignore: one_member_abstracts
 abstract class UseCase<Type, Params> {
   Future<Either<Failure, Type>> execute(Params params);
 }
@@ -11,7 +14,6 @@ abstract class UseCase<Type, Params> {
 // This will be used by the code calling the use case whenever the use case
 // doesn't accept any parameters.
 class NoParams extends Equatable {
-
   @override
   List<Object?> get props => [];
 }
