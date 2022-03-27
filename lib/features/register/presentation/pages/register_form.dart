5ed53feb9db8e2b07eeed770730f5c13a310e2a2
madhavth <powermt2012@gmail.com>
2022-03-27 08:07:55 +0545

    fixed missing lint issues

--- lib/features/register/presentation/pages/register_form.dart
+++ lib/features/register/presentation/pages/register_form.dart
@@ -1,26 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_start_repo/core/widgets/button.dart';
-import 'package:flutter_start_repo/core/widgets/loading.dart';
-import 'package:flutter_start_repo/features/register/presentation/cubits/register_bloc.dart';
-import 'package:flutter_start_repo/features/register/presentation/cubits/register_state.dart';
-import 'package:flutter_start_repo/utils/router.dart';
+
+import '../../../../core/widgets/button.dart';
+import '../../../../core/widgets/loading.dart';
+import '../../../../utils/router.dart';
+import '../cubits/register_bloc.dart';
+import '../cubits/register_state.dart';
 
 class RegisterForm extends StatefulWidget {
+  const RegisterForm({Key? key}) : super(key: key);
+
   @override
   _RegisterFormState createState() => _RegisterFormState();
 }
 
 class _RegisterFormState extends State<RegisterForm> {
-
   late GlobalKey<FormState> _formKey;
 
-  _onRegisterUser() {
-    if (!_formKey.currentState!.validate()) return;
+  void _onRegisterUser() {
+    if (!_formKey.currentState!.validate()) {
+      return;
+    }
     _formKey.currentState!.save();
     BlocProvider.of<RegisterBloc>(context).registerUser();
   }
-
 
   @override
   void initState() {
@@ -31,39 +34,40 @@ class _RegisterFormState extends State<RegisterForm> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SingleChildScrollView(
-      child: Column(
-        // mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          SizedBox(
-            height: 64,
-          ),
-          FlutterLogo(
-            size: 128,
-          ),
-          SizedBox(
-            height: 64,
-          ),
-          BlocConsumer<RegisterBloc, RegisterState>(
-            listener: (context, state) {
-              if (state is RegisterSuccess) {
-                Navigator.pushReplacementNamed(context, AppRouter.LOGIN);
-              } else if (state is RegisterError) {
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(
-                    content: Text('${state.error}'),
-                    backgroundColor: Colors.red,
-                  ),
-                );
-              }
-            },
-            builder: (context, state) {
-              return _buildBody(state);
-            },
-          )
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          // mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const SizedBox(
+              height: 64,
+            ),
+            const FlutterLogo(
+              size: 128,
+            ),
+            const SizedBox(
+              height: 64,
+            ),
+            BlocConsumer<RegisterBloc, RegisterState>(
+              listener: (context, state) {
+                if (state is RegisterSuccess) {
+                  Navigator.pushReplacementNamed(context, AppRouter.LOGIN);
+                } else if (state is RegisterError) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text(state.error),
+                      backgroundColor: Colors.red,
+                    ),
+                  );
+                }
+              },
+              builder: (context, state) {
+                return _buildBody(state);
+              },
+            )
+          ],
+        ),
       ),
-    ));
+    );
   }
 
   Widget _buildBody(RegisterState state) {
@@ -74,14 +78,14 @@ class _RegisterFormState extends State<RegisterForm> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            SizedBox(
+            const SizedBox(
               height: 24,
             ),
             state is RegisteringState
-                ? LoadingIndicator()
+                ? const LoadingIndicator()
                 : Button(
                     title: 'Sign Up',
-                    onPressed: this._onRegisterUser,
+                    onPressed: _onRegisterUser,
                   )
           ],
         ),
